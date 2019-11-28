0c714b56dd1402f1419013a38aa5c973008040c1
lokjok <54335243+lokjok@users.noreply.github.com>
2019-11-28 10:10:42 +0100

    div. sec, IMM, EMM - initial

--- ion_temperature_plots/get_mathi_traj.jl
+++ ion_temperature_plots/get_mathi_traj.jl
@@ -5,7 +5,9 @@
 using PhysicalConstants.CODATA2018
 using Unitful
 
-function get_mathi_traj(Vrf, Udc, Ω, T, E_ext, phi, tspan)
+function get_mathi_traj(Vrf, Udc, Ω, T, E_ext, phi, tspan, div=false)
+    # funkce vraci matici rychlosti a poloh iontu [vx,vy,vz,x,y,z] pro dane casy
+    # input: E_ext - externi DC pole, phi - poc. faze trajektorii, tspan - casovy vektor
 
     # konstanty
     m = 40 * convert(Float64,AtomicMassConstant / (1u"kg")) # hmotnost iontu
@@ -30,26 +32,42 @@ function get_mathi_traj(Vrf, Udc, Ω, T, E_ext, phi, tspan)
     u0 = sqrt.(2*BoltzmannConstant/(u"J*K^-1")*T./(m*ω.^2)) # amplitudy sekularniho pohybu
     us = e./(m*ω.^2) .* E_ext
 
-    # funkce vracejici polohu a rychlost v case t
-    u(t) = (us + u0.*cos.(ω*t + phi)) .* (1 .+ q/2 .* cos(Ω*t))
-    vu(t) = -u0.*ω.*sin.(ω*t + phi) - 1/2*cos(Ω*t)*q.*ω.*u0.*sin.(ω*t+phi) - 1/2*Ω*
-        sin(Ω*t)*u0.*q.*cos.(ω*t + phi) - 1/2*Ω*sin(Ω*t)*q.*us
+    if div
+        # funkce vracejici polohu a rychlost sec. poh, IMM, EMM
+        u_sec(t) = u0.*cos.(ω*t + phi) + us # sekularni pohyb
+        u_IMM(t) = u0.*cos.(ω*t + phi) .* q/2 .* cos(Ω*t) # IMM - pozor na rozdeleni IMM a EMM pro velka us
+        u_EMM(t) = us .* q/2 .* cos(Ω*t) # EMM
+        vu_sec(t) = -u0.*ω.*sin.(ω*t + phi)
+        vu_IMM(t) = - 1/2*cos(Ω*t)*q.*ω.*u0.*sin.(ω*t+phi) - 1/2*Ω*
+            sin(Ω*t)*u0.*q.*cos.(ω*t + phi)
+        vu_EMM(t) = - 1/2*Ω*sin(Ω*t)*q.*us
 
-    # analiticke reseni
-    u_an = u.(tspan)
-    vu_an = vu.(tspan)
+        # reseni
+        
 
-    # finalni usporadani matice u
-    u_return = zeros(length(tspan), 6)
+    else
+        # funkce vracejici polohu a rychlost v case t
+        u(t) = (us + u0.*cos.(ω*t + phi)) .* (1 .+ q/2 .* cos(Ω*t))
+        vu(t) = -u0.*ω.*sin.(ω*t + phi) - 1/2*cos(Ω*t)*q.*ω.*u0.*sin.(ω*t+phi) - 1/2*Ω*
+            sin(Ω*t)*u0.*q.*cos.(ω*t + phi) - 1/2*Ω*sin(Ω*t)*q.*us
 
-    for i in 1:length(tspan)
-        for j in 1:3
-            u_return[i, j] = vu_an[i][j]
-            u_return[i, j+3] = u_an[i][j]
+        # analiticke reseni
+        u_an = u.(tspan)
+        vu_an = vu.(tspan)
+
+        # finalni usporadani matice u
+        u_return = zeros(length(tspan), 6)
+
+        for i in 1:length(tspan)
+            for j in 1:3
+                u_return[i, j] = vu_an[i][j]
+                u_return[i, j+3] = u_an[i][j]
+            end
         end
+
+        return u_return
     end
 
-    return u_return
 end
 
 function get_E_kin_1D(u::Array{Float64,1})
